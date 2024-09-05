586378c94a010541f27bd973ed70b5240dc15508
lkondrotas <kondrlukas@gmail.com>
2024-09-05 16:43:06 +0300

    Clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Container, Typography } from '@mui/material';
+import { Container } from '@mui/material';
 import Header from './header';
 
 const inter = Inter({ subsets: ['latin'] });
