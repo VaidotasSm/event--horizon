a2814f94ef16fe335e198ab9ab4a56724835cfa3
lkondrotas <kondrlukas@gmail.com>
2024-09-05 14:21:25 +0300

    changed href
    Changed href to point to main page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -48,7 +48,7 @@ export default function Header() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
@@ -100,7 +100,7 @@ export default function Header() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
