0f2934ba9b92275ecc601da7e6ed23e175e3c233
lkondrotas <kondrlukas@gmail.com>
2024-09-05 11:54:23 +0300

    Added search input
    Added simple search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Box, FormControl, InputLabel, TextField } from '@mui/material';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>BODY</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
+      <TextField id="search-bar" label="Search" fullWidth />
     </main>
   );
 }
