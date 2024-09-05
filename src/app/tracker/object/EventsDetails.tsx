8f1838c0331151e3368fdfcdff5d86089943c830
lkondrotas <kondrlukas@gmail.com>
2024-09-05 20:00:34 +0300

    Moved table components
    Moved table components from object-grid to tracker/object

--- src/app/tracker/object/EventsDetails.tsx
+++ src/app/tracker/object/EventsDetails.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { TrackingEventDto } from '@/app/server/dto';
+import { TableCell, TableRow } from '@mui/material';
 import React from 'react';
 
 export const EventDetails: React.FC<{ event: TrackingEventDto }> = (props) => (
-  <div>
-    {props.event.createdAt} - {props.event.eventName}
-  </div>
+  <TableRow key={props.event.id}>
+    <TableCell>{props.event.eventName}</TableCell>
+    <TableCell component="th" scope="row">
+      {props.event.description}
+    </TableCell>
+    <TableCell>{props.event.createdAt}</TableCell>
+  </TableRow>
 );
--- src/app/tracker/object/EventsTable.tsx
+++ src/app/tracker/object/EventsTable.tsx
@@ -1,17 +1,108 @@
 'use client';
 
 import { TrackingEventDto } from '@/app/server/dto';
+import {
+  Box,
+  Collapse,
+  IconButton,
+  Paper,
+  styled,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 import { EventDetails } from './EventsDetails';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export const EventsTable: React.FC<{ events: TrackingEventDto[] }> = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  if (props.events.length === 0) {
+    return (
+      <Typography variant="h4" color="warning">
+        Object has no events
+      </Typography>
+    );
+  }
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+  }));
+
   return (
-    <ul>
-      {props.events.map((e) => (
-        <li key={e.createdAt}>
-          <EventDetails event={e} />
-        </li>
-      ))}
-    </ul>
+    <TableContainer sx={{ pb: 2 }} component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell />
+            <StyledTableCell align="left">ID</StyledTableCell>
+            <StyledTableCell align="left">Type</StyledTableCell>
+            <StyledTableCell align="left">Object name</StyledTableCell>
+            <StyledTableCell align="center">Last Change</StyledTableCell>
+            <StyledTableCell align="right">Last Changed</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+            <TableCell>
+              <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+              </IconButton>
+            </TableCell>
+            <TableCell align="left" component="th" scope="row">
+              {props.events[0].id}
+            </TableCell>
+            <TableCell align="left">{props.events[0].objectType}</TableCell>
+            <TableCell align="left">{props.events[0].eventName}</TableCell>
+            <TableCell align="center">{props.events[0].objectChanges.amount}</TableCell>
+            <TableCell align="right">{props.events[0].createdAt}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <Box sx={{ margin: 1 }}>
+                  <Typography variant="h6" gutterBottom component="div">
+                    History
+                  </Typography>
+                  <Table size="small" aria-label="purchases">
+                    <TableHead>
+                      <TableRow>
+                        <StyledTableCell>Name</StyledTableCell>
+                        <StyledTableCell>Change</StyledTableCell>
+                        <StyledTableCell>Change date</StyledTableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {props.events.map((row) => (
+                        <EventDetails key={row.id} event={row} />
+                      ))}
+                    </TableBody>
+                  </Table>
+                </Box>
+                {/* <EventDetails event={row} /> */}
+              </Collapse>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+    // <ul>
+    //   {props.events.map((e) => (
+    //     <li key={e.createdAt}>
+    //       <EventDetails event={e} />
+    //     </li>
+    //   ))}
+    // </ul>
   );
 };
