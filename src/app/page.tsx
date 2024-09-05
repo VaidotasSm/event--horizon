99a2f1be0c1076367e4dc9258fa9feda9c12f62a
lkondrotas <kondrlukas@gmail.com>
2024-09-05 12:54:47 +0300

    Added style to search bar
    Styled search bar, added button and event listeners for input change and button click
    
    Also installed icons-material

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
-import { Box, FormControl, InputLabel, TextField } from '@mui/material';
+'use client';
+
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Home() {
+  const handleClickSearch = () => {
+    console.log('search');
+  };
+
+  const handleSearchChnage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-24">
-      <TextField id="search-bar" label="Search" fullWidth />
+      <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
+        <InputLabel htmlFor="search-bar">Search</InputLabel>
+        <OutlinedInput
+          id="search-bar"
+          type="text"
+          onChange={handleSearchChnage}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton aria-label="Search" onClick={handleClickSearch} edge="end">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Search"
+        />
+      </FormControl>
     </main>
   );
 }
